f49214ce6d321a6f81b2d079d069a491d15124e5
Viktoriia <beloborodova.viktoriia@gmail.com>
2024-06-12 10:16:37 +0300

    updated build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import NavMenu from './components/NavMenu';
 import Home from './pages/Home';
@@ -7,21 +7,20 @@ import Projects from './pages/Projects';
 import Contacts from './pages/Contacts';
 import Footer from './components/Footer';
 import SmoothScrollbar from './components/SmoothScrollbar';
-import {HashRouter} from 'react-router-dom';
 
 export default function App() {
   return (
-      <HashRouter>     
+    <HashRouter>     
       <NavMenu />
       <SmoothScrollbar>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contacts" element={<Contacts />} />
-          </Routes>
-          <Footer />
-          </SmoothScrollbar>
-      </HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </SmoothScrollbar>
+    </HashRouter>
   );
 };
